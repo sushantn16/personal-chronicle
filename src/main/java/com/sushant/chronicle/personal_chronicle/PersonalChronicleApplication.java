83d0b833db8e87cfccd8c6456b78c37427c696fb
sushantn16 <nandwani160698@gmail.com>
2025-04-19 13:31:20 +0200

    Initial comit. setup spring

---
+++ src/main/java/com/sushant/chronicle/personal_chronicle/PersonalChronicleApplication.java
@@ -0,0 +1,13 @@
+package com.sushant.chronicle.personal_chronicle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalChronicleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalChronicleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sushant/chronicle/personal_chronicle/PersonalChronicleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sushant.chronicle.personal_chronicle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalChronicleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
